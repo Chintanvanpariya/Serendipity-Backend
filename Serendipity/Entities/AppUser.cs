671d4d675c9c59782174cc9d4b2e3330a07d6a8d
chintan5588@gmail.com <Chintanvanpariya>
2021-01-10 14:57:30 -0600

    Add project files.

---
+++ Serendipity/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Serendipity.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+
+    }
+}
